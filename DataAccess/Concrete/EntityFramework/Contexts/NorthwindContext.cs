77a7ee57b48479f2a48586081affa44e0c727928
ysfcndgr <ycandogru@gmail.com>
2021-07-02 01:44:22 +0300

    Main

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;initial catalog=northwind;integrated security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Entities/Dtos/DomainModels/Cart.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Dtos.DomainModels
+{
+    public class Cart:IDomainModel
+    {
+        public Cart()
+        {
+            CartLines = new List<CartLine>();
+        }
+        public List<CartLine> CartLines { get; set; }
+
+      
+    }
+}
